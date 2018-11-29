0dfa77d724de2e418a29c7292d11c3ff329897ea
candydasein <michaellorensiegel@gmail.com>
2018-11-29 10:42:28 -0500

    Fixed routes
    Pushing to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
-  # patch '/change-password' => 'users#changepw'
+  patch '/change-password' => 'users#changepw'
 end
